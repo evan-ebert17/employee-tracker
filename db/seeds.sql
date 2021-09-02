693413603113ec53adfca3132780e6759209ea36
EvanEbert <evan.ebert17@gmail.com>
2021-09-02 04:06:05 -0500

    fixed corresponding decimal and manager_id issue

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,20 +6,20 @@ VALUES ("Germ Eating"),
 
 INSERT INTO roles (title, salary, department_id)
         -- Germ
-VALUES  ("Germ Manager", 150000.00, 1),
-        ("Germ Maker", 91800.00, 1),
-        ("Germ Conniser", 80000.80, 1),
+VALUES  ("Germ Manager", 150000, 1),
+        ("Germ Maker", 91800, 1),
+        ("Germ Conniser", 80000, 1),
         --  Pencil
-        ("Pencil Manager", 125999.00, 2),
-        ("Master Pusher ", 92000.00, 2),
-        ("Pencil Itself", 30000.50, 2),
+        ("Pencil Manager", 125999, 2),
+        ("Master Pusher ", 92000, 2),
+        ("Pencil Itself", 30000, 2),
         --  Jello
-        ("Jello Manager", 170000.00, 3),
-        ("Jello Maker", 120000.50, 3),
-        ("Jello Analyst ", 75000.00, 3),
+        ("Jello Manager", 170000, 3),
+        ("Jello Maker", 120000, 3),
+        ("Jello Analyst ", 75000, 3),
         --  Pikmin
-        ("Biggest Pikmin Fan ", 190000.50, 4),
-        ("Moderate Pikmin Fan ", 90000.00, 4),
+        ("Biggest Pikmin Fan ", 190000, 4),
+        ("Moderate Pikmin Fan ", 90000, 4),
         ("Pikmin Unjoyer", 100, 4);
 
 INSERT INTO employees (first_name, last_name, role_id)
